25970a9b8ea8307c1f8776a76f18be354c9df808
huilight <3243148844@qq.com>
2019-03-08 16:17:24 +0800

    配置static文件路径

--- donghubbs/settings.py
+++ donghubbs/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_DIRS = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -122,5 +122,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATICFILES_DIRS = [STATIC_DIRS, ]
 STATIC_URL = '/static/'
